a26ab9751cb953b685eeaa86979cb0855de29d0d
Bunkhim <bunkhim09@gmail.com>
2026-02-06 09:18:24 +0700

    add my route

---
+++ lib/config/routes.dart
@@ -0,0 +1,9 @@
+class AppRoutes {
+  static String splashscreen = "/splash_screen";
+  static String onboardingscreen = "/onboarding_screen";
+  static String registerScreen = "/register";
+  static String loginScreen = "/login";
+  static String verifyEmailScreen = "/verify-email";
+  static String tellUsAboutYouScreen = "/tell-us-about-you";
+  static String forgetPasswordScreen = "/forget_password_screen";
+}
